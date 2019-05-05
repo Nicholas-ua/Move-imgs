5746a07ded2252a1d242b173fbba078d08aa0972
Nicholas-ua <m.savych@gmail.com>
2019-05-05 19:30:58 +0300

    Show img step-01

---
+++ show.js
@@ -0,0 +1,8 @@
+document.getElementById('drag2').addEventListener('mouseup', hideDivs);
+function hideDivs()
+{
+    document.getElementById('drag1').style.display = 'none';
+    document.getElementById('drag2').style.display = 'none';
+    document.getElementById('step01').style.display = 'block';
+    this.style.display = 'none';
+}
