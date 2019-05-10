d12c1b411ff1b4c656ec6761f6a4f03912c0ba17
Nicholas-ua <m.savych@gmail.com>
2019-05-10 22:42:43 +0300

    Change function 01

--- show.js
+++ show.js
@@ -1,8 +1,8 @@
-document.getElementById('drag2').addEventListener('mouseup', hideDivs);
+/*document.getElementById('drag2').addEventListener('mouseup', hideDivs);*/
 function hideDivs()
 {
     document.getElementById('drag1').style.display = 'none';
     document.getElementById('drag2').style.display = 'none';
-    document.getElementById('step01').style.display = 'block';
-    this.style.display = 'none';
+    document.getElementById('step01').src = "img/step-01.png";
+    /*this.style.display = 'none';*/
 }
