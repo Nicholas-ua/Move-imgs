752f231027f8f54a28097f532fa315d457516d38
Nicholas-ua <m.savych@gmail.com>
2019-07-11 12:06:04 +0300

    Add new function

--- show.js
+++ show.js
@@ -1,10 +1,22 @@
 /*document.getElementById('drag2').addEventListener('mouseup', hideDivs);*/
-function hideDivs()
+function hideDivs0()
 {
     document.getElementById('drag1').style.display = 'none';
-    document.getElementById('drag2').style.display = 'none';
-    document.getElementById('step01').style.display = 'block';
-    /*this.style.display = 'none';*/
+    document.getElementById('step00').style.display = 'block';
+}
+
+function hideDivs()
+{
+    if (document.getElementById('step00').style.display === 'block')
+    {
+        document.getElementById('drag1').style.display = 'none';
+        document.getElementById('step00').style.display = 'none';
+        document.getElementById('drag2').style.display = 'none';
+        document.getElementById('step01').style.display = 'block';
+        /*this.style.display = 'none';*/
+    }
+    else
+        alert('Try another part!');
 }
 
 function hideDivs2()
