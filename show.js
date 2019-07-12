a752e3bea430e039f0c5bef98a70114881a24ea0
Nicholas-ua <m.savych@gmail.com>
2019-07-12 15:47:54 +0300

    All funtions

--- show.js
+++ show.js
@@ -30,3 +30,64 @@ function hideDivs2()
     else
         alert('Try another part!');
 }
+
+function hideDivs3()
+{
+    if (document.getElementById('step02').style.display === 'block')
+    {
+        document.getElementById('step02').style.display = 'none';
+        document.getElementById('drag4').style.display = 'none';
+        document.getElementById('step03').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs4()
+{
+    if (document.getElementById('step03').style.display === 'block')
+    {
+        document.getElementById('step03').style.display = 'none';
+        document.getElementById('drag5').style.display = 'none';
+        document.getElementById('step04').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs5()
+{
+    if (document.getElementById('step04').style.display === 'block')
+    {
+        document.getElementById('step04').style.display = 'none';
+        document.getElementById('drag6').style.display = 'none';
+        document.getElementById('step05').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs6()
+{
+    if (document.getElementById('step05').style.display === 'block')
+    {
+        document.getElementById('step05').style.display = 'none';
+        document.getElementById('drag7').style.display = 'none';
+        document.getElementById('step06').style.display = 'block';
+    }
+    else
+        alert('Try another part!');
+}
+
+function hideDivs7()
+{
+    if (document.getElementById('step06').style.display === 'block')
+    {
+        document.getElementById('step06').style.display = 'none';
+        document.getElementById('drag8').style.display = 'none';
+        document.getElementById('step07').style.display = 'block';
+        document.getElementById('message').innerHTML = 'Congratulation! You have assembled the Robot!';
+    }
+    else
+        alert('Try another part!');
+}
